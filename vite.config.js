2fc9ed3a13bbb32027ef35159e93dbbed53920db
Kashish Saxena <kashish.saxena.123056@gmail.com>
2026-03-18 10:33:38 -0700

    link

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+<<<<<<< HEAD
 
+=======
+base: "https://github.com/Kashish9717/MyAlbum",
+>>>>>>> 61d9d2e (aa)
 })
